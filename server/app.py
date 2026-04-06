8e4f1530d5c9f01d02072217a2d977a4c4ca1b30
Spandan <spandansatapute019@gmail.com>
2026-04-06 21:04:15 +0530

    added main function

--- server/app.py
+++ server/app.py
@@ -181,3 +181,11 @@ def health():
     return {
         "status": "healthy"
     }
+    
+    import uvicorn
+
+def main():
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    main()
